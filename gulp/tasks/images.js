af4b561b76b37992598c210529dc65c4732fabbc
andrew1337 <andrey.h@cattle-care.com>
2023-01-19 17:07:14 +0700

    fix: commenting imagemin

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -34,17 +34,17 @@ export const images = () => {
 				app.plugins.newer(app.path.build.images)
 			)
 		)
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				imagemin({
-					progressive: true,
-					svgoPlugins: [{ removeViewBox: false }],
-					interlaced: true,
-					optimizationLevel: 3 // 0 to 7
-				})
-			)
-		)
+		// .pipe(
+		// 	app.plugins.if(
+		// 		app.isBuild,
+		// 		imagemin({
+		// 			progressive: true,
+		// 			svgoPlugins: [{ removeViewBox: false }],
+		// 			interlaced: true,
+		// 			optimizationLevel: 3 // 0 to 7
+		// 		})
+		// 	)
+		// )
 		.pipe(app.gulp.dest(app.path.build.images))
 		.pipe(app.plugins.browsersync.stream());
 }
