23493850b6fdb68778c085ad0b4b467449ae0344
Yanina <yamamporiya@wallarm.com>
2023-02-03 17:42:00 +0700

    add new field to crm

--- src/js/collectData.js
+++ src/js/collectData.js
@@ -1,4 +1,6 @@
 const modals = document.querySelectorAll('.modal');
+const tabs = document.querySelectorAll('.modal_tabs_item')
+const types = ['full', 'gift'];
 
 // modal windows can't be scrolled
 modals.forEach(modal => {
@@ -6,40 +8,33 @@ modals.forEach(modal => {
     modal.addEventListener('touchmove', (event) => event.preventDefault());
 });
 
-// close modals by click on blur
-const blur = document.querySelector('.blur')
-blur.addEventListener('click', () => {
+// close foo
+const onClose = () => {
     modals.forEach(modal => modal.classList.add('hidden'))
-})
 
-// nav by tabs
-const tabs = document.querySelectorAll('.modal_tabs_item')
+    document.querySelectorAll('.modal_content')
+        .forEach(slide => {
+            if (!slide.classList.contains('min_phone_number')) slide.classList.add('hidden')
+        })
+    document.querySelectorAll(`.modal_content.car_model`).forEach(item => item.classList.remove('hidden'))
+
+    tabs.forEach(tab => tab.classList.remove('checked'))
+    types.forEach(type => document.getElementById(`tab-${type}-0`)?.classList.add('checked'));
+}
 
 // modal windows closes with data-value of close btn
 const closeBtns = document.querySelectorAll('.modal_close');
-closeBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        modals.forEach(modal => {
-            if (modal.classList.contains(btn.dataset.value) || modal.classList.contains('blur')) modal.classList.add('hidden')
-        })
+closeBtns.forEach(btn => btn.addEventListener('click', onClose));
 
-        document.querySelectorAll('.modal_content')
-            .forEach(slide => {
-                if (!slide.classList.contains('min_phone_number')) slide.classList.add('hidden')
-            })
-        document.querySelector(`.modal_content.car_model`).classList.remove('hidden')
-
-        tabs.forEach(tab => tab.classList.remove('checked'))
-        document.getElementById(`tab-0`).classList.add('checked')
-
-    })
-});
+// close modals by click on blur
+const blur = document.querySelector('.blur')
+blur.addEventListener('click', onClose)
 
 // modal form
 const textInputs = document.querySelectorAll('.form_input');
-const services = document.querySelectorAll('input[name="service"]');
-const carUsed = document.querySelectorAll('input[name="used_car"]');
-const gifts = document.querySelectorAll('input[name="gift"]');
+const services = types.map(type => Array.from(document.querySelectorAll(`input[name="${type}_service"]`))).flat();
+const carUsed = types.map(type => Array.from(document.querySelectorAll(`input[name="${type}_used_car"]`))).flat();
+const gifts = types.map(type => Array.from(document.querySelectorAll(`input[name="${type}_gift"]`))).flat();
 const userAgreements = document.querySelectorAll('input[value="agree"]');
 
 // pre added value of service
@@ -49,8 +44,13 @@ window.onload = () => {
         if (item.value.includes(window.location.pathname
                     .replaceAll("/","")
                     .replaceAll(".html",""))) {
+
             item.checked = true;
-            document.getElementById(`for_service`).classList.remove('disabled');
+            types.map(type => {
+                const nextBtn = document.getElementById(`for_${type}_service`)
+                nextBtn.classList.remove('disabled');
+                nextBtn.disabled = false;
+            });
         }
     })
 }
@@ -72,7 +72,7 @@ const onInputChange = (input, valueLength) => {
 const onSelectChange = (arr, input) => {
     const btn = document.getElementById(`for_${input.name}`);
 
-    if (Array.from(arr).some(input => input.checked)) {
+    if (Array.from(arr).some(e => e.name === input.name && e.checked)) {
         btn.disabled = false
         btn.classList.remove('disabled')
     }
